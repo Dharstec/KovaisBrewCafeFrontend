ea4c6fd6d4e33ecdaf017a9a3c24d76ac0fd0fbf
Gokul <gokuldsp01@gmail.com>
2026-01-05 19:52:01 +0530

    bill

--- routes/billing.js
+++ routes/billing.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/billing.js");
 const { verifyToken } = require("../middleware/auth.js");
 
 router.post("/bills", verifyToken, controller.createBill);
-router.post("/complete/:id", verifyToken, controller.completeBill);
+router.post("/bill/complete/:id", verifyToken, controller.completeBill);
 router.get("/pending", verifyToken, controller.pendingBills);
 router.get("/completed", verifyToken, controller.completedBills);
 router.put("/bills/:id", verifyToken, controller.updateBill);
